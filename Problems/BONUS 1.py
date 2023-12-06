10f62645b664aeb85a1b249cf92574f2b731ee44
dngvmnh <dngvmnh@gmail.com>
2023-12-07 01:04:08 +0700

    Update BONUS 1.py

--- Problems/BONUS 1.py
+++ Problems/BONUS 1.py
@@ -1,15 +1,4 @@
-def printA(h):
-    w = 2 * h - 1
-    for i in range(h):
-        for j in range(w):
-            if (i == h // 2) or (i + j == h - 1) or (j - i == h - 1):
-                print('*', end="")
-            elif (i == (h // 2)-1) or (i + j == h) or (j - i == h - 2):
-                print('*', end="")
-            else:
-                print(' ', end="")
-        print()
-def printAA():
+def printA():
     print(" "*15 + "A"*3 + " "*15)
     for i in range(4):
         print(" "*(14-i)+ "A" + ":"*(3+2*i)+ "A"+" "*(14-i))
@@ -22,10 +11,99 @@ def printAA():
     for i in range(3):
         print(" "*(3-i)+ "A" + ":"*5 + "A" + " "*(13+2*i) + "A" + ":"*5 + "A" + " "*(3-i))
     print("A"*7+ " "*19 +"A"*7)
+    print("\n")
 
+def printB():
+    print("B" + ":"*5 + "B"*11 + ":"*5 + "B")
+    for i in range(6):
+        print("B" + ":"*5 + "B" + " "*11 + "B" + ":"*5 + "B")
+    print("B" + ":"*5 + "B"*11 + ":"*5 + "B")
+    for i in range(7):
+        print("B" + ":"*5 + "B" + " "*11 + "B" + ":"*5 + "B")
+    print("B" + ":"*5 + "B"*11 + ":"*5 + "B")
+    print("\n")
+
+def printD():
+    print("D" + ":"*5 + "D"*9 + ":"*5 + "D")
+    for i in range(14):
+        print("D" + ":"*5 + "D" + " " * 9 + "D" + ":"*5 + "D")
+    print("D" + ":"*5 + "D"*9 + ":"*5 + "D")
+    print("\n")
+
+def printE():
+    print("E" + ":"*9 + "E")
+    for i in range(7):
+        print("E" + " " * 11 + "E")
+    print("E" + ":"*9 + "E")
+    for i in range(7):
+        print("E" + " " * 11 + "E")
+    print("E" + ":"*9 + "E")
+    print("\n")
+
+def printF():
+    print("F" + ":"*9 + "F")
+    for i in range(7):
+        print("F" + " " * 11 + "F")
+    print("F" + ":"*9 + "F")
+    for i in range(6):
+        print("F" + " " * 11 + "F")
+    print("F" + " " * 11 + "F")
+    print("\n")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def printR():
+    print(" " + "R"*23 + " ")
+    for i in range(6):
+        print("R" + ":"*5 + "R" + " "*11 + "R" + ":"*5 + "R")
+    print("R" + ":"*5 + "R" + " "*3 + " " + " "*3 + "R" + ":"*5 + "R")
+    for i in range(7):
+        print("R" + ":"*5 + "R" + " "*(2*i+1) + "R" + ":"*5 + "R")
+    print("\n")
+    
 if __name__ == "__main__":
-    # h = int(input("height: "))
-    # while h < 20:
-    #     h = int(input("height >= 20: "))
-    # printA(h)
-    printAA()
+    # printA()
+    # printB()
+    # printC()
+    # printD()
+    printE()
+    printF()
+    # printG()
+    # printH()
+    # printI()
+    # printJ()
+    # printK()
+    # printL()
+    # printM()
+    # printN()
+    # printO()
+    # printP()
+    # printQ()
+    # printR()
+    # printS()
+    # printT()
+    # printU()
+    # printV()
+    # printW()
+    # printX()
+    # printY()
+    # printZ()
